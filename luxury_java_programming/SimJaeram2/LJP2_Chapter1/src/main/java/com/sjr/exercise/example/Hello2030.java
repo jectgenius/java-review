9be9f50e210f285be03779a50af73bd697b460c7
Sim Jaeram <68117213+jectgenius@users.noreply.github.com>
2024-08-22 09:31:45 +0900

    Feat(SimJaeram2): 1장 자바 시작 실습 #24

---
+++ luxury_java_programming/SimJaeram2/LJP2_Chapter1/src/main/java/com/sjr/exercise/example/Hello2030.java
@@ -0,0 +1,8 @@
+package com.sjr.exercise.example;               // 패키지 선언문
+
+public class Hello2030 {                        // Hello2030 클래스 정의 시작
+    public static void main(String[] args) {    // main() 메소드 정의 시작
+        int n = 2030;                           // int형 변수 n 선언 동시에 2030 대입하여 초기화
+        System.out.println("헬로" + n);          // System.out.println() 메소드 호출하여 문자열 "헬로" + n 출력
+    }                                           // main() 메소드 정의 끝
+}                                               // Hello2030 클래스 정의 시작
